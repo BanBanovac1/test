b0a2492bb73e2b189e7e295528b1991c9b38b901
unknown <antebane@outlook.com>
2017-08-23 02:38:20 +0200

    second commit

---
+++ src/app/header.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+
+export default class HeaderComponent extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			width: '0',
+			height: '0',
+			isMobileMenuClosed: true,
+			menuIcon: '../images/hamburger.png'
+		};
+  }
+
+  
+
+	handleClick () {
+		this.setState({isMobileMenuClosed: !this.state.isMobileMenuClosed});
+		if(this.state.isMobileMenuClosed == false) {
+			this.setState({menuIcon: '../images/closeIcon.png'});
+			document.getElementsByClassName('headerTopMenu')[0].style.display = 'block';
+			document.getElementsByTagName('html')[0].style.overflowY = 'hidden';
+		}
+		else {
+			this.setState({menuIcon: '../images/hamburger.png'});
+			document.getElementsByTagName('html')[0].style.overflowY = 'scroll';
+			document.getElementsByClassName('headerTopMenu')[0].style.display = 'none';
+			
+		}	
+	}
+
+
+
+    render() {
+        return (
+			<div className="header">
+				<div className="headerTop">
+					<div className="headerTopTitle">
+						<h1>EXPLOREcroatia</h1>
+					</div>
+					<div className="headerTopCallImg">
+						<img src="../images/call.png" alt="call" /> 
+					</div>
+					<div className="headerTopPhone">
+						+(0)385 91 5386 369
+					</div>
+					<ul className="headerTopMenu">
+						<li>Home</li>
+						<li>Boats</li>
+						<li>Apartments</li>
+						<li>Explore</li>
+						<li>Contact</li>
+					</ul>
+					<img src={this.state.menuIcon} className="menuIcon"  alt="menu" onClick={this.handleClick.bind(this)} />
+				</div>
+				<div className="headerMain">
+					<div className="headerMainTitle">
+						<h2>New adventure every day!</h2> 
+					</div>
+					<div className="headerMainText">
+						Book homes, boats & trips to the prettiest destinations in the world.
+						Experience places and fell real beauty of Croatian coast.
+					</div>
+					<div className="headerMainMenuList">
+						<div className="headerMainMenuListItem">
+							<img className="boatIcon" src="../images/boatIcon.png" alt="boat" />
+							<div className="headerMainMenuListItemText">Choose boat type</div>
+							<img className="backIcon" src="../images/backIcon.png" alt="back" />
+						</div>
+						<div className="headerMainMenuListItem">
+							<img className="peopleIcon" src="../images/peopleIcon.png" alt="boat" />
+							<div className="headerMainMenuListItemText">Number of people</div>
+							<img className="backIcon" src="../images/backIcon.png" alt="back" />
+						</div>
+						<div className="headerMainMenuListItem">
+							<img className="dateIcon" src="../images/dateIcon.png" alt="boat" />
+							<div className="headerMainMenuListItemText">Starting date</div>
+							<img className="backIcon" src="../images/backIcon.png" alt="back" />
+						</div>
+						<div className="headerMainMenuListItem">
+							<img className="dateIcon" src="../images/dateIcon.png" alt="boat" />
+							<div className="headerMainMenuListItemText">Ending date</div>
+							<img className="backIcon" src="../images/backIcon.png" alt="back" />
+						</div>
+						<div className="headerMainMenuListButton">Search</div>
+					</div>
+				</div>
+				<div className="headerVideo">
+					<div className="headerVideoText">WATCH VIDEO</div>
+					<div className="headerVideoImg">
+						<img className="playIcon" src="../images/playIcon.png" alt="play" />
+					</div>
+				</div>
+			</div>
+        );
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,70 +1,126 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-require('../styles/main.scss');
+require('../styles/header.scss');
+require('../styles/booking.scss');
+
+import HeaderComponent from './header.js';
 
 //Create component
-class TodoComponent extends React.Component{
+class AppComponent extends React.Component{
 	
 	render() {
-		
-		return(
-			<div className="header">
-				<div className="headerTop">
-					<div className="headerTopTitle">
-						<h1>EXPLOREcroatia</h1>
+		return (
+			<div className="app">
+				<HeaderComponent />
+				<div className="boatCard">
+					<div className="boatCardImg">
+						<img src="../images/boat.png" alt="boat" />	
 					</div>
-					<div className="headerTopCallImg">
-						<img src="../images/call.png" alt="call" /> 
+					<div className="boatName">Jeanneau Merry Fisher 456</div>
+					<div className="boatAddress">Kaštel Gomilica, Marka Marulića...</div>
+					<div className="boatCardRowOne">
+						<img className ="boatCardPeopleIcon" src="../images/peopleIcon.png" alt="boatIcon" />
+						<div className="boatCardRowText">8 people</div>
+						<img className ="boatCardSpeedIcon" src="../images/speedIcon.png" alt="boatIcon" />
+						<div className="boatCardRowText">70 knots</div>
 					</div>
-					<div className="headerTopPhone">
-						+(0)385 91 5386 369
+					<div className="boatCardRowTwo">
+						<img className ="boatCardBedIcon" src="../images/bedIcon.png" alt="boatIcon" />
+						<div className="boatCardRowText">4 beds</div>
+						<img className ="boatCardWrenchIcon" src="../images/wrenchIcon.png" alt="boatIcon" />
+						<div className="boatCardRowText">Built in 2017</div>
 					</div>
-					<ul className="headerTopMenu">
-						<li>Home</li>
-						<li>Boats</li>
-						<li>Apartments</li>
-						<li>Explore</li>
-						<li>Contact</li>
-					</ul>
-					<img src="../images/hamburger.png" className="menuIcon"  alt="menu" />
-				</div>
-				<div className="headerMain">
-					<div className="headerMainTitle">
-						<h2>New adventure every day!</h2> 
-					</div>
-					<div className="headerMainText">
-						Book homes, boats & trips to the prettiest destinations in the world.
-						Experience places and fell real beauty of Croatian coast.
-					</div>
-					<div className="headerMainMenuList">
-						<div className="headerMainMenuListItem">
-							<img className="boatIcon" src="../images/boatIcon.png" alt="boat" />
-							<div className="headerMainMenuListItemText">Choose boat type</div>
-							<img className="backIcon" src="../images/backIcon.png" alt="back" />
+					<div className="boatCardPriceButtonAlign">
+						<div className="boatCardPriceBox">
+							<div className="boatCardPrice">320<span>€</span></div>
+							<div className="boatCardPriceText">per day</div>
 						</div>
-						<div className="headerMainMenuListItem">
-							<img className="peopleIcon" src="../images/peopleIcon.png" alt="boat" />
-							<div className="headerMainMenuListItemText">Number of people</div>
-							<img className="backIcon" src="../images/backIcon.png" alt="back" />
-						</div>
-						<div className="headerMainMenuListItem">
-							<img className="dateIcon" src="../images/dateIcon.png" alt="boat" />
-							<div className="headerMainMenuListItemText">Starting date</div>
-							<img className="backIcon" src="../images/backIcon.png" alt="back" />
-						</div>
-						<div className="headerMainMenuListItem">
-							<img className="dateIcon" src="../images/dateIcon.png" alt="boat" />
-							<div className="headerMainMenuListItemText">Ending date</div>
-							<img className="backIcon" src="../images/backIcon.png" alt="back" />
-						</div>
-						<div className="headerMainMenuListButton">Search</div>
+						<a href="#" className="boatCardButton">Book Now</a>
 					</div>
 				</div>
-				<div className="headerVideo">
-					<div className="headerVideoText">WATCH VIDEO</div>
-					<div className="headerVideoImg">
-						<img className="playIcon" src="../images/playIcon.png" alt="play" />
+				<div className="bookTourAlign">
+					{/*BOOK TOUR*/}
+					<div className="bookTourCard">
+						<div className="bookTourCardTop">
+							<div className="bookTourCardTopTitle">TOTAL PRICE:</div>
+							<div className="bookTourCardTopPriceBox">
+								<div className="bookTourPrice">360<span>€</span></div>
+								<div className="bookTourPriceText">120€ per person</div>
+							</div>
+						</div>
+						<div className="bookTourPeopleSelect">
+							<img className="bookTourPeopleIcon" src="../images/peopleIcon.png" alt="people" />
+							<div className="bookTourPeopleSelectText">Number of people: </div>
+							<div className="bookTourPeopleSelectNumberBox">
+								<img className="bookTourMinus" src="../images/minusIcon.png" alt="minus" />
+								<div className="bookTourPeopleNumber">3</div>
+								<img className="bookTourPlus" src="../images/plusIcon.png" alt="plus" />
+							</div>
+						</div>
+						<div className="bookTourDateSelect">
+							<img className="bookTourDateIcon" src="../images/dateIcon.png" alt="date" />
+							<div className="bookTourDateSelectText">Select date: </div>
+							<div className="bookTourDateSelectDatePicker">
+								<div className="bookTourDateSelectDate">21st of July, <span>Sunday</span></div>
+								<img className="bookTourBackIcon" src="../images/backIcon.png" alt="back" />
+							</div>
+						</div>
+						<a href="#" className="bookTourButton">Book now</a>
+						<div className="bookTourHelp">Have a question? Call <span>+(0) 385 91 5386 369</span></div>
+					</div>
+					{/*BOAT BOOKING*/}
+					<div className="bookBoatCard">
+						<div className="bookBoatCardTop">
+							<div className="bookBoatCardTopTitle">TOTAL PRICE:</div>
+							<div className="bookBoatCardTopPriceBox">
+								<div className="bookBoatPrice">360<span>€</span></div>
+								<div className="bookBoatPriceText">Insurance included</div>
+							</div>
+						</div>
+						<div className="bookBoatCardCheckDate">
+							<div className="checkDateBox">
+								<img className="dateIcon" src="../images/dateIcon.png" alt="date" />
+								<div className="checkDateText">Check in date</div>
+								<img className="backIcon" src="../images/backIcon.png" alt="back" />
+							</div>
+							<div className="checkDateBox right">
+								<img className="dateIcon" src="../images/dateIcon.png" alt="date" />
+								<div className="checkDateText">Check out date</div>
+								<img className="backIcon" src="../images/backIcon.png" alt="back" />
+							</div>
+						</div>
+						<div className="bookBoatPeopleSelect">
+							<img className="bookBoatPeopleIcon" src="../images/peopleIcon.png" alt="people" />
+							<div className="bookBoatPeopleSelectText">Number of people: </div>
+							<div className="bookBoatPeopleSelectNumberBox">
+								<img className="bookBoatMinus" src="../images/minusIcon.png" alt="minus" />
+								<div className="bookBoatPeopleNumber">3</div>
+								<img className="bookBoatPlus" src="../images/plusIcon.png" alt="plus" />
+							</div>
+						</div>
+						<div className="bookBoatSkipperSelect">
+							<img className="steeringIcon" src="../images/steeringIcon.png" alt="steering" />
+							<div className="skipperSelectText">Skipper</div>
+							<div className="skipperSelectChoiceTrue">yes</div>
+							<div className="skipperSelectChoiceFalse">no</div>
+							<div className="skipperSelectPriceBox">
+								<div className="skipperSelectPrice">320<span>€</span></div>
+								<div className="skipperSelectText">per day</div>
+							</div>
+						</div>
+						<div className="bookBoatPetsSelect">
+							<img className="petsIcon" src="../images/petsIcon.png" alt="steering" />
+							<div className="petsSelectText">Pets</div>
+							<div className="petsSelectChoiceFalse">yes</div>
+							<div className="petsSelectChoiceTrue">no</div>
+							<div className="petsSelectPriceBox">
+								<div className="petsSelectPrice">12<span>€</span></div>
+								<div className="petsSelectText">per day</div>
+							</div>
+						</div>
+						<a href="#" className="bookBoatButton">Book now</a>
+						<div className="bookBoatHelp">Have a question? Call <span>+(0) 385 91 5386 369</span></div>
 					</div>
 				</div>
 			</div>
@@ -75,4 +131,4 @@ class TodoComponent extends React.Component{
 
 
 //put component into html pageX
-ReactDOM.render(<TodoComponent />, document.getElementById('app-wrapper'));
+ReactDOM.render(<AppComponent />, document.getElementById('app-wrapper'));
